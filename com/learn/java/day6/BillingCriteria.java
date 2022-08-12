79dff8193a6643ae9dd5fdd173741d360076b359
“swatisk99” <swatisk1990@yahoo.com>
2022-08-12 11:20:36 +0530

    day 6

--- com/learn/java/day6/BillingCriteria.java
+++ com/learn/java/day6/BillingCriteria.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 
 public class BillingCriteria {
-
+	
 	public static void main(String[] args) {
-
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter units consumed");
+		float unit=sc.nextFloat(),bill;
+		if (unit<=100)
+			bill=0;
+		if(unit<=300)
+			bill=(unit-100)*2.5f;
+		else if(unit<=500)
+			bill=500+(unit-300)*3;
+		else if(unit<=1000)
+			bill=1100+(unit-500)*5;
+		else
+			bill=3600+(unit-1000)*7;
+		System.out.print("Bill amount\t: Rs"+bill);
 	}
 
 }
