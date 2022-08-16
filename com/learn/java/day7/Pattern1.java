314a1c88d255466b7540ee8b50d2f9fc58c0de29
“swatisk99” <swatisk1990@yahoo.com>
2022-08-16 16:22:44 +0530

    day7 patterns

---
+++ com/learn/java/day7/Pattern1.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter number : ");
+		int num=s.nextInt();
+		int i=1,j;
+		while(i<=num)
+		{	j=i;
+			while(j>0) {
+				System.out.print("* ");
+				j--;
+			}i++;
+			System.out.print("\n");
+		}
+	}
+
+}
---
+++ com/learn/java/day7/Pattern3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter number : ");
+		int num=s.nextInt();
+		int i=1,j,val=1;
+		System.out.print("Enter Increment Order : ");
+		int increment=s.nextInt();
+		while(i<=num)
+		{	j=1;
+			while(j<=i) {
+				System.out.print(val+" ");
+				j++;
+				val+=increment;
+			}i++;
+			val=1;
+			System.out.print("\n");
+		}
+	}
+}
