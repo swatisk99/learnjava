d706baa0309e3a6a79affc7ec29ec92fa9ab5356
“swatisk99” <swatisk1990@yahoo.com>
2022-08-30 15:11:24 +0530

    minor modifications

--- com/learn/java/day8/OneDArray.java
+++ com/learn/java/day8/OneDArray.java
@@ -1,21 +1,27 @@
 package com.learn.java.day8;
 
+
 public class OneDArray {
 
 	public static void main(String[] args) {
-		int array[]= {1,2,3,5,7,8,10,14,17};
-		int i,big1=array[0],big2=array[0];
+		long[] array= {1,2,10,14,17,-3,5,7,8};
+		int i;
+		long big2=array[0];
+		long big1=array[0];
 		for(i=0;i<9;i++)
 			System.out.println("array["+i+"] = "+array[i]);
-		for(int j:array)
+		for(long j:array)
 			big1=big1<j?j:big1;
-		for(int j:array)
+		for(long j:array)
 			big2=((big2<j)&&(big1>j))?j:big2;
-		System.out.print(big2+" is the second largest number.");
+		System.out.println(big2+" is the second largest number.");
 		System.out.println("Elements of array in reverse : ");
 		for(i=array.length-1;i>=0;i--)
 			System.out.print(array[i]+" ");
-			
+		SortArray sort=new SortArray();
+		array=sort.getAscendingOrder(array);
+		System.out.println("\n"+array[2]+" is the third smallest number.");
+		
 	
 	}
 
--- com/learn/java/day8/strings/FindString.java
+++ com/learn/java/day8/strings/FindString.java
@@ -1,10 +1,38 @@
 package com.learn.java.day8.strings;
 
+import java.util.Scanner;
+
 public class FindString {
-
+	public static void findMatch(String str, String substring, int i) {
+		int j=0,substringLength=substring.length();
+		while(j<substringLength) {
+			if(str.charAt(i)==substring.charAt(j)) {
+				j++;i++;
+			}
+			else
+				break;
+		}
+		if(j==substringLength)
+			System.out.println(substring+" present in \""+str+"\" from "+(i-substringLength+1)+"th postion.");
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter string");
+		String str=scan.nextLine();
+		System.out.print("Enter substring to find");
+		String substring = scan.nextLine();
+		int i=0,stringLength=str.length(),substringLength=substring.length();
+		
+		while(i<=(stringLength-substringLength)) {
+			if(str.charAt(i)==substring.charAt(0)) {
+				findMatch(str,substring,i);
+			}
+			i++;
+		}
+		if(i>(stringLength-substringLength+1))
+			System.out.print("Subtring not found");
+		
+			
 	}
 
 }
