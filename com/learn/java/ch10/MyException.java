d066d900f434fa352d8dca488e5679a2c088cf5b
swatisk99 <swatisk1990@yahoo.com>
2022-10-11 13:35:16 +0530

    Exceptions

--- com/learn/java/ch10/MyException.java
+++ com/learn/java/ch10/MyException.java
@@ -3,8 +3,6 @@ package com.learn.java.ch10;
 public class MyException extends Exception {
 //Have all methods available to Throwable. May override. 
 	
-	//private static final long serialVersionUID = 4122944237257270374L;	//Useful in case of serialization
-
 	String message;
 	public MyException(String name){
 		message =  "Name should contain atleast 3 letters "+name;
